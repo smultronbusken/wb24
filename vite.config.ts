fccd532dae1559e1dd5585e246f9a53801c36277
smultron <fredholmisak@outlook.com>
2024-01-25 15:54:30 +0100

    proper vite config for buffer package

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,22 @@
 import { defineConfig } from 'vite';
 import path from "path"
 import react from '@vitejs/plugin-react';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 import Inject from '@rollup/plugin-inject'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), 
-      /*NodeModulesPolyfillPlugin(
-        {
-          name: "Buffer"
-        }
-      ),
-      Inject( {
-        modules: {
-          Buffer: ['buffer', 'Buffer']
-        },
-      })*/
-    ],
+    plugins: [react() ],
     base: '/wb24/',
     resolve: {
         alias: {
           "@": path.resolve(__dirname, "./src"),
-          'Buffer': 'rollup-plugin-node-polyfills/polyfills/Buffer',
         },
-        
     },
-   optimizeDeps: {
-      esbuildOptions: {
-        define: {
-          global: 'globalThis',
-        },
-        plugins: [
-          NodeGlobalsPolyfillPlugin({
-            buffer: true,
-          }),
-        ],
-      },
-    },
-    /*
+
     build: {
-      commonjsOptions: { transformMixedEsModules: true } // Change
-    }*/
+      commonjsOptions: { transformMixedEsModules: true }, // Change
+      rollupOptions: {
+        plugins: [Inject({ Buffer: ['buffer', 'Buffer'] })],
+    },
+    }
 });
